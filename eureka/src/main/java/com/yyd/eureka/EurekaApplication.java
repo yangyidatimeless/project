7e9a76c1d97d2a4ef14497789b414e022746db24
yangyida <yang_yida@163.com>
2019-03-12 15:27:25 +0800

    syso

--- eureka/src/main/java/com/yyd/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/yyd/eureka/EurekaApplication.java
@@ -10,6 +10,7 @@ public class EurekaApplication {
 	public static void main(String[] args) {
 		//eureka hello local  print
 		SpringApplication.run(EurekaApplication.class, args);
+		System.out.println("I'm started");
 	}
 
 }
