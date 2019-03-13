8ffe55cc44250f5cc7568ae555ab06b709fd6665
yangyida <yang_yida@163.com>
2019-03-13 14:57:22 +0800

    print

--- eureka/src/main/java/com/yyd/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/yyd/eureka/EurekaApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaApplication {
 	public static void main(String[] args) {
-		//eureka
+		//eureka print
 		SpringApplication.run(EurekaApplication.class, args);
 		System.out.println("I'm started");
 		System.out.println("I want effected");
