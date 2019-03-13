6de5576438f375e24124b32cac2e4deef1cfd8e4
yangyida <yang_yida@163.com>
2019-03-13 14:44:22 +0800

    effected

--- eureka/src/main/java/com/yyd/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/yyd/eureka/EurekaApplication.java
@@ -8,9 +8,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaApplication {
 	public static void main(String[] args) {
-		//eureka ecipse
+		//eureka test 
 		SpringApplication.run(EurekaApplication.class, args);
 		System.out.println("I'm started");
+		System.out.println("I want effected");
 	}
 
 }
