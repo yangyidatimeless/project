ace5cb6501be6c2a6ac5a69969076928a2cb748b
yangyida <yang_yida@163.com>
2019-03-13 15:40:52 +0800

    正式

--- eureka/src/main/java/com/yyd/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/yyd/eureka/EurekaApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaApplication {
 	public static void main(String[] args) {
+		//eureka
 		SpringApplication.run(EurekaApplication.class, args);
 	}
 }
